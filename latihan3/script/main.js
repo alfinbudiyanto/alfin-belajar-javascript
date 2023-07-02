6829e09f4d4c0c318724a2abe1595cde91124aec
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2023-07-03 01:54:52 +0700

    penghitungan matematika selesai-3.5

--- latihan3/script/main.js
+++ latihan3/script/main.js
@@ -1,50 +1,67 @@
-const user = prompt(
-  "Pilih antara Lingkaran, Segitiga, Persegi panjang atau Jajargenjang!"
-).toLowerCase();
+let pertanyaan = true;
 
-/*
-if (user === "lingkaran") {
-  console.log("rumus lingkaran");
-} else if (user === "segitiga") {
-  console.log("rumus segitiga");
-} else if (user === "persegi panjang") {
-  console.log("rumus persegi panjang");
-} else if (user === "jajargenjang") {
-  console.log("rumus jajargenjang");
-} else {
-  console.log("pilih antara ke-empat pilihan itu euy!");
-}
-*/
+while (pertanyaan) {
+  const user = prompt(
+    "Pilih antara Lingkaran, Segitiga, Persegi panjang atau Jajargenjang!"
+  ).toLowerCase();
 
-switch (user) {
-  case "lingkaran":
-    const jariJariLingkaran = prompt(
-      "Masukkan panjang jari-jari lingkarannya!"
-    );
+  switch (user) {
+    case "lingkaran":
+      const jariJari = tanya("Masukkan panjang jari-jari lingkarannya cuy!");
+      lingkaran(jariJari);
+      break;
 
-    hitungLuasLingkaran(jariJariLingkaran);
-    break;
+    case "segitiga":
+      const alasSG = tanya("Masukkan panjang alas segitiganya cuy!");
+      const tinggiSG = tanya("Masukkan tinggi segitiganya cuy!");
+      segitiga(alasSG, tinggiSG);
+      break;
 
-  case "segitiga":
-    console.log("rumus segitiga");
-    break;
+    case "persegi panjang":
+      const panjang = tanya("Masukkan panjang dari persegi panjangnya cuy!");
+      const lebar = tanya("Masukkan lebar persegi panjangnya cuy!");
+      persegiPanjang(panjang, lebar);
+      break;
 
-  case "persegi panjang":
-    console.log("rumus persegi panjang");
-    break;
+    case "jajargenjang":
+      const alasJG = tanya("Masukkan panjang alas jajargenjangnya cuy!");
+      const tinggiJG = tanya("Masukkan tinggi jajargenjangnya cuy!");
+      jajargenjang(alasJG, tinggiJG);
+      break;
 
-  case "jajargenjang":
-    console.log("rumus jajargenjang");
-    break;
+    default:
+      alert("pilih salah satu sesuai instruksi brooo...!");
+      break;
+  }
 
-  default:
-    console.log("pilih salah satu sesuai instruksi brooo...!");
-    break;
+  pertanyaan = confirm("Menghitung lagi?");
 }
 
 // Fungsi untuk Matematika
-function hitungLuasLingkaran(jariJari) {
-  const luasLingkaran = (22 / 7) * jariJari * jariJari;
+// Lingkaran
+function lingkaran(jariJari) {
+  if (jariJari % 7 === 0) {
+    return alert(`Luas lingkaran = ${(22 / 7) * jariJari * jariJari}`);
+  }
+  return alert(`Luas lingkaran = ${3.14 * jariJari * jariJari}`);
+}
 
-  console.log(luasLingkaran);
+// Segitiga
+function segitiga(a, t) {
+  return alert(`Luas segitiga = ${0.5 * a * t}`);
+}
+
+// Persegi panjang
+function persegiPanjang(p, l) {
+  return alert(`Luas persegi panjang = ${p * l}`);
+}
+
+// Jajargenjang
+function jajargenjang(a, t) {
+  return alert(`Luas jajargenjang = ${a * t}`);
+}
+
+// Untuk mengubah tipe string ke integer & menampilkan pertanyaan
+function tanya(tanya) {
+  return parseInt(prompt(tanya));
 }
